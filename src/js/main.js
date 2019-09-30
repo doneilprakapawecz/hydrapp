51a15161ede50cc81ca4c0fab762fb545a0e4436
daniel <prokopowiczdaniel@gmail.com>
2019-09-30 09:02:36 +0200

    wersja bez local storage

--- src/js/main.js
+++ src/js/main.js
@@ -19,4 +19,10 @@ if ('serviceWorker' in navigator) {
 
 console.log(`Hello world!`);
 
+const button_1=document.querySelector('.button-1--js');
+button_1.addEventListener("click",(e)=>{number.innerHTML=number.innerHTML*1+1;});
+const button_2=document.querySelector('.button-2--js');
+button_2.addEventListener("click",(e)=>{if(number.innerHTML*1>0)number.innerHTML=number.innerHTML*1-1;});
+const number=document.querySelector('.number--js');
+
 
