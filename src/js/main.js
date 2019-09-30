23a3067ac49f9d462075df076f05e82292c35070
daniel <prokopowiczdaniel@gmail.com>
2019-09-30 09:58:57 +0200

    wersja z local

--- src/js/main.js
+++ src/js/main.js
@@ -19,10 +19,31 @@ if ('serviceWorker' in navigator) {
 
 console.log(`Hello world!`);
 
+
+
 const button_1=document.querySelector('.button-1--js');
-button_1.addEventListener("click",(e)=>{number.innerHTML=number.innerHTML*1+1;});
+button_1.addEventListener("click",(e)=>{
+ 
+ if(localStorage.getItem('numer'))
+  {
+    number.innerHTML=localStorage.getItem('numer')*1+1;
+    localStorage.setItem('numer',number.innerHTML);
+  }
+  else
+  {
+    localStorage.setItem('numer','1');
+    number.innerHTML=number.innerHTML*1+1;
+  }
+});
 const button_2=document.querySelector('.button-2--js');
-button_2.addEventListener("click",(e)=>{if(number.innerHTML*1>0)number.innerHTML=number.innerHTML*1-1;});
+button_2.addEventListener("click",(e)=>{
+  if(number.innerHTML*1>0)
+  {
+    number.innerHTML=localStorage.getItem('numer')*1-1;
+    localStorage.setItem('numer',number.innerHTML);
+  }
+});
 const number=document.querySelector('.number--js');
+if(localStorage.getItem('numer'))number.innerHTML=localStorage.getItem('numer');
 
 
